d2430353ab5dfc33037566db52efb4ecc1ee7e59
magicalpsy <magicalpsy@naver.com>
2020-06-26 15:54:13 +0900

    kubernetes folder and sh file upload

---
+++ k8s/k8s.sh
@@ -0,0 +1,6 @@
+kubectl run ubuntu-nginx --image=magicalpsy/ubuntu_nginx --port=80
+kubectl get deployments
+kubectl expose deployment/ubuntu-nginx --type="NodePort" --port 80
+kubectl scale deployment ubuntu-nginx --replicas=20
+kubectl get deployments
+kubectl describe deployments/ubuntu-nginx
